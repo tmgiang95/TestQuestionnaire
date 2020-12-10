76df31df60b1d0dd66f74fd1dc85c25e5c4c2c7c
MinhGiang <tmgiang95>
2020-12-10 11:01:07 +0700

    init

---
+++ app/src/main/java/com/example/testquestionnaire/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.testquestionnaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.questionnairelibrary.CameraActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getBaseContext(), CameraActivity.class);
+        startActivity(intent);
+    }
+}
